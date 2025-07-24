9fa7c847ec2117f262f43db93e1b9622166a9e4f
B4TM4N-0 <batmangrahhh@gmail.com>
2025-07-25 02:14:40 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import webhookCommand from "../commands/webhook";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user?.tag}`);
+});
+
+client.on("messageCreate", message => {
+  if (message.content === "!webhook") {
+    webhookCommand(message);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
