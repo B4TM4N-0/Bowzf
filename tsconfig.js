720705cd95b07144eb6fa589f7c51cb42ff503a3
B4TM4N-0 <batmangrahhh@gmail.com>
2025-07-25 02:16:25 +0800

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,11 @@
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "commonjs",
+    "outDir": "dist",
+    "rootDir": ".",            // now looks at the whole folder
+    "strict": true,
+    "esModuleInterop": true
+  },
+  "include": ["index.ts", "commands"]
+}
