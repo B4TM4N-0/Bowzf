e1abb353b955b72cf1fcbf10235ddd407be2c002
B4TM4N-0 <batmangrahhh@gmail.com>
2025-07-25 02:35:54 +0800

    Update webhook.ts

--- commands/webhook.ts
+++ commands/webhook.ts
@@ -1,3 +1,5 @@
+import { Message } from "discord.js";
+
 export default function webhookCommand(message: Message) {
   if ("send" in message.channel) {
     message.channel.send("Webhook command executed!");
