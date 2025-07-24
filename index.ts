c046fac747a43bed6918282a365a136414606883
B4TM4N-0 <batmangrahhh@gmail.com>
2025-07-25 06:01:04 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Message } from "discord.js";
+import embedCommand from "./commands/embed";
 
+const OWNER_ID = process.env.OWNER_ID;
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent // <-- This is required!
+    GatewayIntentBits.MessageContent,
   ],
 });
 
@@ -13,6 +15,16 @@ client.once("ready", () => {
   console.log(`✅ Logged in as ${client.user?.tag}`);
 });
 
+client.on("messageCreate", async (message: Message) => {
+  if (message.author.bot) return;
+  if (message.author.id !== OWNER_ID) return;
+  if (!message.content.startsWith("/")) return;
 
+  const [cmd] = message.content.slice(1).split(" ");
+
+  if (cmd === "embed") {
+    await embedCommand(message);
+  }
+});
 
 client.login(process.env.DISCORD_TOKEN);
